77a4645c1ad626d798676c5914aa95f60aa69418
amanneox <amanadhikari2@gmail.com>
2018-04-24 22:34:37 +0530

    new

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
-var app=new Vue({
-  el: '#app',
-  data: () => ({
-    title: "card",
-  }),
-  created: function() {
-    let vm = this
-    fetch('instagram.json').then((response) => {
-      return response.json().then((json) => {
-        console.log("JSON", json)
-        vm.data = json
-      })
-    })
-  }
-})
+window.onload = function () {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      object: {"BTC":{"url":"http://www.anotherindian.com/wp-content/uploads/2017/08/instagram-photos-depression.jpg","text":3759.91},
+                    "ETH":{"url": "http://static5.uk.businessinsider.com/image/59e9d9f6cb45a027008b4a2b-960/elma-field.png",
+                  "text":281.7},
+                    "NEW Currency":{"url": "https://blog.ink361.com/wp-content/uploads/2015/02/jcgolfing.jpg",
+                      "text":5.60}
+
+        }
+    }
+  })
+};
