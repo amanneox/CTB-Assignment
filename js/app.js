b620ad66864c4e12a3bd4cddafa86eac576e9e6b
amanneox <amanadhikari2@gmail.com>
2018-04-25 11:47:08 +0530

    new

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ window.onload = function () {
                   "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi."},
                     "3":{"url": "https://blog.ink361.com/wp-content/uploads/2015/02/jcgolfing.jpg",
                       "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi."},"4":{"url": "https://s-i.huffpost.com/gadgets/slideshows/404432/slide_404432_5031960_free.jpg",
-                    "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi."},"5":{"url": "https://s-i.huffpost.com/gadgets/slideshows/404432/slide_404432_5031960_free.jpg",
+                    "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi."},"5":{"url": "https://static.boredpanda.com/blog/wp-content/uploads/2016/09/True-Living-instagram-will-make-you-rethink-about-your-daily-life-perspective-57e272647e470__700.jpg",
                   "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi."},"6":{"url": "https://s-i.huffpost.com/gadgets/slideshows/404432/slide_404432_5031960_free.jpg",
                 "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi."},
 
