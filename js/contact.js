ebbe26e429f5cbbee9b6388915d1be529e7f9b0a
amanneox <amanadhikari2@gmail.com>
2018-04-26 20:09:20 +0530

    new

---
+++ js/contact.js
@@ -0,0 +1,41 @@
+$(function() {
+
+  $("form[name='ctb']").validate({
+
+    rules: {
+
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+
+        email: true
+      },
+      message: {
+        required: true,
+        minlength: 5
+      }
+    },
+
+    messages: {
+      firstname: "We need your firstname",
+      lastname: "We need your lastname",
+      message: {
+        required: "Sorry, Your mesage can't be empty",
+        minlength: "Your message must be at least 5 characters long"
+      },
+      email: "Please use a valid e-mail address"
+    },
+
+    invalidHandler: function(form, validator) {
+      $('#failed').show();
+     },
+    submitHandler: function(form) {
+      form.submit();
+      $('#myform').remove();
+        $('#failed').remove();
+      $('#success').show();
+      e.preventDefault();
+    }
+  });
+});
